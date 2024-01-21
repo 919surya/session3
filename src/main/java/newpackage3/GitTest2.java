c1f2e035b8eadda16312b2c762d49a8d6631589f
919surya <sbb.bkc@gmail.com>
2024-01-21 09:27:37 -0500

    2nd commit

---
+++ src/main/java/newpackage3/GitTest2.java
@@ -0,0 +1,16 @@
+package newpackage3;
+
+public class GitTest2 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("git Test");
+
+	}
+	
+	public void printMe() {
+		System.out.println("git Test 1");
+		
+	}
+
+}
